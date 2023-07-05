8d27be1fac420afa6b4e0f6122d073d98aa59136
EZow25 <evanzhao90@gmail.com>
2023-07-05 13:32:27 -0400

    inital push

--- PipeSnake/PipeSnakeSim3D/driver.py
+++ PipeSnake/PipeSnakeSim3D/driver.py
@@ -4,14 +4,18 @@ import numpy as np
 from pipe import Pipe
 
 def main():
-    p = Pipe(5)      
-    p.add_straight_pipe((0,0,0), 50)
-    p.add_straight_pipe((0,50,0), 70)
+    p = Pipe(5, (0,0,0))      
+    p.add_straight_pipe(50)
+    p.add_straight_pipe(70)
 
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
-    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.5, 1, 0.5, 1]))
+    # ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([0.5, 1, 0.5, 1]))
     ax.plot_surface(p.x, p.y, p.z, alpha=0.5)
+    plt.title("Ultrasound Visualization")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
     plt.show()
     plt.close(fig=fig)
 
--- PipeSnake/PipeSnakeSim3D/pipe.py
+++ PipeSnake/PipeSnakeSim3D/pipe.py
@@ -1,26 +1,85 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import transforms3d as t3d
 
 class Pipe:
-    def __init__(self, radius):
+    def __init__(self, radius, start_pt):
         self.radius = radius
         self.x = np.zeros((1,1))
         self.y = np.zeros((1,1))
         self.z = np.zeros((1,1))
-    
+        self.start_pt = start_pt
+        self.direction = "+y"
+        # direction specifies what axis to extend the pipe and in what direction along the axis
+        # default is +y
+            
     # Draws a straight pipe
-    # center is the (x,y,z) of the start of the center of the pipe
-    # radius is the radius of the pipe
     # length is the length of the pipe
-    def add_straight_pipe(self, center, length):
-        center_x = center[0]
-        center_y = center[1]
-        center_z = center[2]
-        y_pts = np.linspace(center_y, center_y + length,30)
-        theta = np.linspace(0, 2*np.pi,30)
-        theta_grid, y_grid=np.meshgrid(theta, y_pts)
-        x_grid = self.radius*np.cos(theta_grid) + center_x
-        z_grid = self.radius*np.sin(theta_grid) + center_z
+    def add_straight_pipe(self, length):
+        # get point to start the pipe
+        center_x = self.start_pt[0]
+        center_y = self.start_pt[1]
+        center_z = self.start_pt[2]
+        
+        # form x, y, z point arrays
+        x_grid = np.array(0)
+        y_grid = np.array(0)
+        z_grid = np.array(0)
+        if self.direction == "+y":
+            y_pts = np.linspace(center_y, center_y + length, 30)
+            theta = np.linspace(0, 2*np.pi,30)
+            theta_grid, y_grid=np.meshgrid(theta, y_pts)
+            x_grid = self.radius*np.cos(theta_grid) + center_x
+            z_grid = self.radius*np.sin(theta_grid) + center_z
+            
+            self.start_pt = (center_x, center_y + length, center_z)
+            
+        elif self.direction == "-y":
+            y_pts = np.linspace(center_y - length, center_y, 30)
+            theta = np.linspace(0, 2*np.pi,30)
+            theta_grid, y_grid=np.meshgrid(theta, y_pts)
+            x_grid = self.radius*np.cos(theta_grid) + center_x
+            z_grid = self.radius*np.sin(theta_grid) + center_z
+            
+            self.start_pt = (center_x, center_y - length, center_z)
+            
+        elif self.direction == "+x":
+            x_pts = np.linspace(center_x, center_x + length, 30)
+            theta = np.linspace(0, 2*np.pi,30)
+            theta_grid, x_grid=np.meshgrid(theta, x_pts)
+            y_grid = self.radius*np.cos(theta_grid) + center_y
+            z_grid = self.radius*np.sin(theta_grid) + center_z
+            
+            self.start_pt = (center_x + length, center_y, center_z)
+             
+        elif self.direction == "-x":
+            x_pts = np.linspace(center_x - length, center_x, 30)
+            theta = np.linspace(0, 2*np.pi,30)
+            theta_grid, x_grid=np.meshgrid(theta, x_pts)
+            y_grid = self.radius*np.cos(theta_grid) + center_y
+            z_grid = self.radius*np.sin(theta_grid) + center_z
+            
+            self.start_pt = (center_x - length, center_y, center_z)
+            
+        elif self.direction == "+z":
+            z_pts = np.linspace(center_z, center_z + length, 30)
+            theta = np.linspace(0, 2*np.pi,30)
+            theta_grid, z_grid=np.meshgrid(theta, z_pts)
+            x_grid = self.radius*np.cos(theta_grid) + center_x
+            y_grid = self.radius*np.sin(theta_grid) + center_y
+            
+            self.start_pt = (center_x, center_y, center_z + length)
+            
+        elif self.direction == "-z":
+            z_pts = np.linspace(center_z - length, center_z, 30)
+            theta = np.linspace(0, 2*np.pi,30)
+            theta_grid, z_grid=np.meshgrid(theta, z_pts)
+            x_grid = self.radius*np.cos(theta_grid) + center_x
+            y_grid = self.radius*np.sin(theta_grid) + center_y
+            
+            self.start_pt = (center_x, center_y, center_z - length)
+        
+        # update local vars for x, y, z points
         if np.array_equal(self.x, np.zeros((1,1))):
             self.x = x_grid
         else:
