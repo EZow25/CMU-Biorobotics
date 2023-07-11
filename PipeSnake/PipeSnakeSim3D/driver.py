hard code fix, 90 degree rotation of cross section

PipeSnake/PipeSnakeSim3D/driver.py
@@ -17,7 +17,7 @@ def main():
     
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
-    # ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([2, 2, 0.5, 2]))
+    ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([1,1,1,1]))
     ax.scatter(p.x, p.y, p.z, alpha=0.5)
     plt.title("PipeSnake Simulation")
     ax.set_xlabel("x (meters)")

PipeSnake/PipeSnakeSim3D/pipe.py
@@ -134,7 +134,7 @@ class Pipe:
                     return T_ru
                 
                 def transform_cross(rotate, translate):
-                    pose = [x, y, 0, 0, 0, math.sin(rotate/2), math.cos(rotate/2)]
+                    pose = [0 - y + 0.33, y + 0.33, 0, 0, 0, math.sin(rotate/2), math.cos(rotate/2)]
                     T = get_transformation1(pose)
                     x_cross_copy = x_cross.copy()
                     y_cross_copy = y_cross.copy()
@@ -145,14 +145,15 @@ class Pipe:
                             x_cross_copy[i][j] = p[0]
                             y_cross_copy[i][j] = p[1]
                             z_cross_copy[i][j] = p[2]
-                
+                    
+                    
                     self.x = np.append(self.x, x_cross_copy, axis=1)
                     self.y = np.append(self.y, y_cross_copy, axis=1)
                     self.z = np.append(self.z, z_cross_copy, axis=1)
                 
                 theta = -np.pi/2
-                transform_cross(theta/2, 0)
-                transform_cross(theta, 0.33)
+                # transform_cross(theta/2, 0)
+                transform_cross(theta, 0)
                 self.start_pt = (x + 0.33, y + 0.33, z)
                 self.direction = "+x"
                 